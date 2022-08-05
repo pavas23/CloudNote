fdaa1038e207ba1061a19512c5c634133b1cdd41
Pavas Garg <pavasgarg@Pavass-MacBook-Pro.local>
2022-08-05 17:43:52 +0530

    changed txt color

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -54,7 +54,7 @@ export default function Notes(props) {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+                            <h5 className="modal-title" id="exampleModalLabel" style={{color:"black"}}>Edit Note</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
